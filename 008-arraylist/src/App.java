f5e3c3f201fc95800f33073da0546d1032348205
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-10-25 14:41:34 -0300

    updates

--- 008-arraylist/src/App.java
+++ 008-arraylist/src/App.java
@@ -20,14 +20,36 @@ public class App {
 
         // Array Estático
         System.out.println("Imprimindo o Array Estático");
+
+        // Imprime o array estático
         for( int indice = 0; indice < arrayEstatico.length; indice++) {
             System.out.println(arrayEstatico[indice]);
         }
 
+        System.out.println("Primeira posição: " + arrayDinamico.get(0));
+
+        // Exibe o último elemento do arrayList
+        int ultimoIndice = arrayDinamico.size() - 1;
+        System.out.println("\nÚltima posição: " + arrayDinamico.get(ultimoIndice));
+
+        // Remove elemento do arrayList
+        arrayDinamico.remove(1);
+        System.out.println("Remover Maria");
+
+        // limpa o arrayList
+        arrayDinamico.clear(); 
+        System.out.println();
+
         System.out.println("\nImprimindo o ArrayList:");
 
+        // Imprime todo o arrayList
         for ( int indice = 0; indice < arrayDinamico.size(); indice++) {
             System.out.println(arrayDinamico.get(indice));
+        }
+
+        // Faz o mesmo processo da linha 46-48 usando for each
+        for (String nome : arrayDinamico) {
+            System.out.println(nome);
         }
 
     }
