87636f5c4073baaf6da5fbe0a1371d5cca23f7e6
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-10-29 20:26:00 -0300

    Update App.java

--- 010-heranca/src/App.java
+++ 010-heranca/src/App.java
@@ -4,12 +4,14 @@ import animais.Gato;
 public class App {
     public static void main(String[] args) throws Exception {
         
+        // Criando o objeto cachorro -> Instanciando a classe Cachorro
         Cachorro cachorro = new Cachorro();
         cachorro.setNome("Luciano");
         cachorro.comer();
         cachorro.beber();
         cachorro.latir();
 
+        // Criando o objeto gato -> Instanciando a classe Gato
         Gato gato = new Gato();
         gato.setNome("Lula");
         gato.beber();
