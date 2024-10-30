5215b026ebba440e8239a612e179f6509ff68c5a
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-10-30 15:12:27 -0300

    updates

--- 011-heranca-2/src/App.java
+++ 011-heranca-2/src/App.java
@@ -5,8 +5,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         Personagem personagem = new Personagem("Joao");
         System.out.println(personagem);
+        personagem.atacar();
 
         Arqueiro arqueiro = new Arqueiro("Varus");
         System.out.println(arqueiro);
+        arqueiro.atacar();
     }
 }
--- 011-heranca-2/src/jogo/Arqueiro.java
+++ 011-heranca-2/src/jogo/Arqueiro.java
@@ -12,10 +12,14 @@ public class Arqueiro extends Personagem {
 
     @Override
     public String toString() {
-
         String infoPersonagem = super.toString();
         String infoArqueiro = String.format(" - Arma: %s", arma);
         return infoPersonagem + infoArqueiro;
+    }
+
+    @Override
+    public void atacar() {
+        System.out.println("O arqueiro atacou com " + arma + "!");
     }
     
 }
