9b3adfcab5c84d0b61cee75c2964bc26de4dcd4b
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-10-30 20:35:49 -0300

    updates

--- 013-polimorfismo/src/App.java
+++ 013-polimorfismo/src/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         Filme filme = new Filme("Lagoa Azul");
         filme.play();
+        filme.play("Português");
     }
 }
--- 013-polimorfismo/src/devflix/Filme.java
+++ 013-polimorfismo/src/devflix/Filme.java
@@ -10,5 +10,9 @@ public class Filme extends Video {
     public void play() {
         System.out.println("Play: filme " + getNome());
     }
+
+    public void play(String idioma) {
+        System.out.println("Play: filme " + getNome() + " no idioma " + idioma);
+    }
     
 }
