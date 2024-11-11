4318343caab396ad46ef786e2db595d52d27f15c
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-11-11 14:06:33 -0300

    Update App.java

--- 015-metodos-estaticos/src/App.java
+++ 015-metodos-estaticos/src/App.java
@@ -1,10 +1,26 @@
 public class App {
+    static int soma = 0;
+    private static void imprimirMensagem() {
+        System.out.println("Olá, mundo!");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Resultado:");
-        
-        System.out.println(Matematica.somar(5, 10));
-        System.out.println(Matematica.calcularAreaCirculo(5));
-        System.out.println(Matematica.calcularAreaQuadrado(5));
+
+        System.out.println("Soma: " + soma);
+        imprimirMensagem();
+
+        Carro ferrari = new Carro("Ferrari", 2024);
+        Carro fusca = new Carro("Fusca", 1999);
+        fusca.setNome("Fusca branco");
+
+        ferrari.anoCompra = 2007;
+        fusca.anoCompra = 2020;
+        Carro.anoCompra = 2077;
+
+        System.out.println(ferrari.getNome());
+        System.out.println(ferrari.anoCompra);
+        System.out.println(fusca.getNome());
+        System.out.println(fusca.anoCompra);
 
     }
 }
