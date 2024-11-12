5a6786934655e41e5c3cd9f7492a82db848b4f61
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-11-12 14:17:29 -0300

    updates

--- 018-try-catch/src/App.java
+++ 018-try-catch/src/App.java
@@ -1,25 +1,30 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int[] numeros = new int[] {20, 30, 40, 100};
-        int[] peso = new int[] {2, 3, 4};
-
+        Matematica matematica = new Matematica();
         
 
-        try { // try -> tentar
-            // tenta executar
-            for (int i = 0; i < 4; i++) {
+        int[] numeros = new int[] {20, 30, 40, 100};
+        int[] peso = new int[] {2, 0, 4};
+
+        for (int i = 0; i < 4; i++) {
+            
+            try { // try -> tentar
+                // tenta executar
+                matematica.dividir(numeros[i], peso[i]);
                 int resultado = numeros[i] / peso[i];
-                System.out.printf("%d / %d = %d \n", numeros[i], peso[i], resultado);
+                System.out.printf("%d / %d = %d \n", numeros[i], peso[i], resultado);  
+                System.out.println("Números: ");
+                System.out.println(numeros[5]);
+            } catch(ArithmeticException erro) {
+                // trata erros de calculos matemáticos
+                System.out.println("Erro de cálculo: " + erro.getMessage());
+            } catch(ArrayIndexOutOfBoundsException erro) {
+                // trata erros de tamanho do vetor
+                System.out.println("Erro de vetor: " + erro.getMessage());
             }
-            System.out.println("Números: ");
-            System.out.println(numeros[5]);
-        } catch(Exception erro) { // catch -> capturar
-            // trata os erros capturados no bloco try
-            // imprimir no console o erro ocorrido
-            // criar um arquivo de log
-            System.err.println("Erro da exceção: " + erro.getMessage());
         }
+        
         System.out.println("Mensagem final");
 
     }
---
+++ 018-try-catch/src/Matematica.java
@@ -0,0 +1,6 @@
+public class Matematica {
+    public void dividir(int numeroA, int numeroB) {
+        int resultado = numeroA / numeroB;
+        System.out.printf("%d / %d = %d", numeroA, numeroB, resultado);
+    }
+}
