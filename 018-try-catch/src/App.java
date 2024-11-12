aa3549477c7604dc24d354a45caedaf005322d72
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-11-12 13:53:22 -0300

    Update App.java

--- 018-try-catch/src/App.java
+++ 018-try-catch/src/App.java
@@ -1,16 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        int[] numeros = new int[] {20, 30, 40, 100};
+        int[] peso = new int[] {2, 3, 4};
+
+        
+
         try { // try -> tentar
             // tenta executar
-            int[] numeros = new int[] {10, 20, 30};
+            for (int i = 0; i < 4; i++) {
+                int resultado = numeros[i] / peso[i];
+                System.out.printf("%d / %d = %d \n", numeros[i], peso[i], resultado);
+            }
             System.out.println("Números: ");
             System.out.println(numeros[5]);
         } catch(Exception erro) { // catch -> capturar
             // trata os erros capturados no bloco try
             // imprimir no console o erro ocorrido
             // criar um arquivo de log
-            System.out.println(erro.getMessage());
+            System.err.println("Erro da exceção: " + erro.getMessage());
         }
         System.out.println("Mensagem final");
 
