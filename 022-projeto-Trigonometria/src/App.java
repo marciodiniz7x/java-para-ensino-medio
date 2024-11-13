d15831e63e34046eec07e05465ecc7d9f82773f3
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-11-13 12:06:04 -0300

    updates

--- 022-projeto-Trigonometria/src/App.java
+++ 022-projeto-Trigonometria/src/App.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Pitagoras pitagoras = new Pitagoras();
-        pitagoras.setCatetoOposto(4);
-        pitagoras.setCatetoAdj(3);
 
-        System.out.println("Hipotenusa = " + pitagoras.calcularHipotenusa());
+        Pitagoras pitagoras = new Pitagoras();
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            // Solicita os valores dos catetos ao usuário
+            System.out.println("Digite o valor do Cateto Oposto:");
+            pitagoras.setCatetoOposto(scanner.nextDouble());
+
+            System.out.println("Digite o valor do Cateto Adjascente:");
+            pitagoras.setCatetoAdj(scanner.nextDouble());
+
+            // Retorna o resultado
+            System.out.printf("Hipotenusa = %.1f ", pitagoras.calcularHipotenusa());
+
+        // Se o usuário digitar um caractere inválido
+        } catch (Exception erro) {
+
+            System.out.println("Você digitou um valor inválido");
+
+        }
     }
 }
