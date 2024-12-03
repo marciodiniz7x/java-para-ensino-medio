b3f47950de55908e2add46c9ca9513b8774c4cef
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-12-03 15:49:37 -0300

    Update Jogo.java

--- 023-jogo-forca/src/Jogo.java
+++ 023-jogo-forca/src/Jogo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Jogo {
@@ -12,11 +13,35 @@ public class Jogo {
     public void iniciarJogo() {
 
         var numeroChutes = 10;
+        var chutes = new ArrayList<String>();
+
+        this.palavra.gerarPalavraSecreta();
 
         var scanner = new Scanner(System.in);
 
         System.out.println("Olá" + this.jogador.getNome() + ", bem-vindo ao Jogo da Forca!");
         System.out.println("Você tem " + numeroChutes + " chutes para adivinhar a palavra secreta.");
         System.out.println(this.palavra.getPalavraComChute() + "\n");
+
+        while (!this.palavra.palavraCompleta() && chutes.size() < numeroChutes) {
+            System.out.println("Digite o seu chute");
+            var chute = scanner.nextLine();
+            if (chutes.contains(chute)) {
+                System.out.println("Você já chutou essa letra. Tente novamente!");
+                continue;
+            }
+            chutes.add(chute);
+            
+            this.palavra.revelarLetra(chute);
+
+            System.out.println("Palavra: " + this. palavra.getPalavraComChute());
+
+            if (this.palavra.palavraCompleta()) {
+                System.out.println("Parabéns " + this.jogador.getNome() + "! Você venceu");
+            } else if (chutes.size() == numeroChutes) {
+                System.out.println("Poxa, " + this.jogador.getNome() + ", você perdeu! A palavra secreta era " + this.palavra.getPalavraSecreta());
+            }
+
+        }
     }
 }
